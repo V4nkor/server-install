b2e6efc857da9fe8564595bcca563a802b394414
Mathieu MORGAT <service.informatique@deltexplan.fr>
2023-08-11 11:16:56 +0200

    WIP - Packet + prev command + doc/help + uninstall

--- server-install.sh
+++ server-install.sh
@@ -1,85 +1,222 @@
 #!/bin/bash
-#Script d'installation customisée de serveur LAMP
+#Basic installation script for most widespread web services and utility packets.
+#Version : 0.2
+#Author : Mathieu Morgat
+#Github page : https://github.com/V4nkor/server-install
 
-#---------Global Functions------------
+currentDirectory="/opt/server-install"
 
-#Install SSH functions and start service
-installSSH(){
-	eval "apt install ssh -y"
-	eval "service ssh start"
-	OUTPUT="$(systemctl status ssh 2>&1)"
-	printf "$OUTPUT \n"
+#Global function
+
+showHelp() {
+	echo "Choose packages and services you wish to install using the dialog windows."
+	echo "To do so use the arrow keys to navigate, use space to select / unselect and press enter to confirm"
+  exit 0
 }
 
-#Install Apache functions and start service
-installApache(){
-	eval "apt-get -y install apache2"
-	eval "a2enmod ssl"
-	eval "a2enmod rewrite"
-	eval "/etc/init.d/apache2 restart"
-	OUTPUT="$(systemctl status apache2 2>&1)"
-        printf "$OUTPUT \n"
-}
-
-#Install PHP functions and start service
-installPHP(){
-	eval "apt-get install php-common libapache2-mod-php php-cli"
-	eval "/etc/init.d/apache2 restart"
-	printf "WIP"
-}
-
-#Install MySQL and setup with base options
-installPHP(){
-	printf "WIP"
-}
-
-#Install GIT
-installGIT(){
-	printf "WIP"
-}
+case "${1}" in
+  "-h" | "help" | "-H")
+    showHelp
+    ;;
+esac
 
 #Install UFW and setup basic rules for web development
 
-#---------Initialise dialog------------
+#---------Initialise packets dialog------------#
 
-cmd=(dialog --separate-output --checklist "Services to install:" 22 76 16)
-options=(
-	1 "SSH" on
-	2 "Apache" on
-	3 "PHP" off
-	4 "MySQL" off
-	5 "GIT" off
-	6 "UFW" off
-)
+	cmd=(dialog --keep-tite --separate-output --checklist "Important packets and utilities :" 22 76 16)
+	options=(
+		nano "nano" on
+		vim "vim" off
+		wget "wget" on
+		curl "curl" on
+		unzip "unzip" on
+		git "git" on
+		ufw "ufw" off
+	)
 
-#Store choices in variable
-choices=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
-clear
+#---------Store packets choices in variable---------#
 
-#Processing selected options
-for choice in $choices
-do
-	case $choice in
-		1)
-			echo "Installing SSH" 
-			installSSH ;;
-		2)
-			echo "Installing Apache" 
-			installApache ;;
-		3)
-			echo "Installing PHP" 
-			installPHP ;;
-		4)
-			echo "Installing MySQL"
-			installMySQL ;;
-		5)
-			echo "Installing GIT"
-			installGIT ;;
-		6)
-			echo "Installing UFW"
-			installUFW ;;
-	esac
-done
+	packets=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
+
+#---------Initialise services dialog------------#
+
+	cmd=(dialog --keep-tite --separate-output --checklist "Web services to install :" 22 76 16)
+	options=(
+		ssh "ssh" on
+		apache2 "apache2" on
+		php "php" on
+		mysql "MySQL" on
+		composer "Composer" on
+	)
+
+#---------Store services choices in variable---------#
+
+	services=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
+
+#---------Packet Functions------------
+
+	#Install nano
+	installNano(){
+		printf '\e[1;31m%-6s\e[m\n' "!!Not yet implemented -> WIP!!"
+	}
+
+	#Install vim
+	installVim(){
+		printf '\e[1;31m%-6s\e[m\n' "!!Not yet implemented -> WIP!!"
+	}
+
+	#Install wget
+	installWget(){
+		printf '\e[1;31m%-6s\e[m\n' "!!Not yet implemented -> WIP!!"
+	}
+
+	#Install curl
+	installCurl(){
+		printf '\e[1;31m%-6s\e[m\n' "!!Not yet implemented -> WIP!!"
+	}
+
+	#Install unzip
+	installUnzip(){
+		printf '\e[1;31m%-6s\e[m\n' "!!Not yet implemented -> WIP!!"
+	}
+
+	#Install git
+	installGit(){
+		printf '\e[1;31m%-6s\e[m\n' "!!Not yet implemented -> WIP!!"
+	}
+
+	#Install ufw
+	installUfw(){
+		printf '\e[1;31m%-6s\e[m\n' "!!Not yet implemented -> WIP!!"
+	}
+
+#---------Processing selected packets---------#
+
+	for packet in $packets
+	do
+		case $packet in
+			nano)
+				printf '\e[1;34m%-6s\e[m\n' "----- Installing nano -----"
+				installNano ;;
+			vim)
+				printf '\e[1;34m%-6s\e[m\n' "----- Installing vim -----" 
+				installVim ;;
+			wget)
+				printf '\e[1;34m%-6s\e[m\n' "----- Installing wget -----" 
+				installWget ;;
+			curl)
+				printf '\e[1;34m%-6s\e[m\n' "----- Installing curl -----"
+				installCurl ;;
+			unzip)
+				printf '\e[1;34m%-6s\e[m\n' "----- Installing unzip -----"
+				installUnzip ;;
+			git)
+				printf '\e[1;34m%-6s\e[m\n' "----- Installing git -----"
+				installGit ;;
+			ufw)
+				printf '\e[1;34m%-6s\e[m\n' "----- Installing ufw -----"
+				installUfw ;;
+		esac
+	done
+
+#---------Services Functions---------#
+
+	#Install SSH functions and start service
+		installSSH(){
+			if (($? == 0)); then eval "apt-get install ssh -y"; fi
+			if (($? == 0)); then eval "service ssh start"; fi
+			OUTPUT="$(systemctl status ssh 2>&1)"
+			printf "$OUTPUT \n"
+		}
+
+	#Install Apache functions and start service
+		installApache(){
+			if (($? == 0)); then eval "apt-get -y install apache2"; fi
+			if (($? == 0)); then eval "a2enmod ssl"; fi
+			if (($? == 0)); then eval "a2enmod rewrite"; fi
+			if (($? == 0)); then eval "/etc/init.d/apache2 restart"; fi
+			OUTPUT="$(systemctl status apache2 2>&1)"
+				printf "$OUTPUT \n"
+		}
+
+	#Install PHP functions and start service
+		installPHP(){
+			if (($? == 0)); then eval "apt-get -y install php-common libapache2-mod-php php-cli"; fi
+			if (($? == 0)); then eval "/etc/init.d/apache2 restart"; fi
+			OUTPUT="$(php --version 2>&1)"
+				printf "$OUTPUT \n"
+		}
+
+	#Install MySQL and setup with base options
+		installMySQL(){
+			#Initialise MySQL dialog
+				cmd=(dialog --separate-output --checklist "MySQL configuration :" 22 76 16)
+				options=(
+					mysql
+				)
+			#Store MySQL choices in variable
+				MySQL=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
+		}
+
+	#Install Composer
+		installComposer(){
+			printf "Not yet implemented -> WIP\n"
+		}
+
+#---------Processing selected services---------#
+
+	for service in $services
+	do
+		case $service in
+			ssh)
+				printf '\e[1;34m%-6s\e[m\n' "----- Installing SSH -----" 
+				installSSH ;;
+			apache)
+				printf '\e[1;34m%-6s\e[m\n' "----- Installing Apache2 -----" 
+				installApache ;;
+			php)
+				printf '\e[1;34m%-6s\e[m\n' "----- Installing PHP -----" 
+				installPHP ;;
+			mysql)
+				printf '\e[1;34m%-6s\e[m\n' "----- Installing MySQL -----"
+				printf '\e[1;31m%-6s\e[m\n' "!!Work in progress!!"
+				#installMySQL 
+				;;
+			composer)
+				printf '\e[1;34m%-6s\e[m\n' "----- Installing Composer -----"
+				installComposer ;;
+		esac
+	done
 
 #Exit script when finished
 exit 0
+
+# Documentation :
+: <<'END_OF_DOCS'
+
+=head1 NAME
+
+Basic installation script for most widespread web services and utility packets.
+
+=head1 SYNOPSIS
+
+server-install [OPTIONS]
+
+=head1 OPTIONS
+	-h = This usage.
+	-v = Be verbose.
+	-V = Show version, copyright, and license information.
+
+=head1 DESCRIPTION
+
+A full description of the application and its features.
+May include numerous subsections (i.e., =head2, =head3, etc.)
+
+[...]
+
+=head1 LICENSE AND COPYRIGHT
+
+=cut
+
+END_OF_DOCS
---
+++ server-uninstall-all.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+#Uninstaller for server-install.sh
+#Version : 0.2
+#Author : Mathieu Morgat
+#Github page : https://github.com/V4nkor/server-install
+
+currentDirectory="/opt/server-install"
+
+#Purge php
+printf '\e[1;34m%-6s\e[m\n' "----- Removing php -----"
+sudo apt purge php-common libapache2-mod-php php-cli
+
+#Purge Apache 2 and it's directories
+printf '\e[1;34m%-6s\e[m\n' "----- Removing apache2 -----"
+sudo apt purge apache2 apache2-utils
+printf '\e[1;34m%-6s\e[m\n' "Removing directories"
+sudo rm -R /usr/sbin/apache2
+sudo rm -R /usr/lib/apache2
+sudo rm -R /etc/apache2
+sudo rm -R /usr/share/apache2 
+
+#Remove all dependencies
+printf '\e[1;34m%-6s\e[m\n' "----- Removing dependencies -----"
+sudo apt-get autoremove
+
+#Delete installation file
+printf '\e[1;34m%-6s\e[m\n' "----- Deleting main script -----"
+cd $currentDirectory
+if (($? == 0)); then sudo rm server-install.sh ; fi
+
+#Delete itself after everything else was uninstalled
+printf '\e[1;34m%-6s\e[m\n' "----- Self deleting uninstaller -----"
+sudo rm -- "$0"
+
+# Documentation :
+: <<'END_OF_DOCS'
+
+=head1 NAME
+
+Uninstaller for server-install.sh
+
+=head1 SYNOPSIS
+
+server-install [OPTIONS]
+
+=head1 OPTIONS
+	-h = This usage.
+	-v = Be verbose.
+	-V = Show version, copyright, and license information.
+
+=head1 DESCRIPTION
+
+A full description of the application and its features.
+May include numerous subsections (i.e., =head2, =head3, etc.)
+
+[...]
+
+=head1 LICENSE AND COPYRIGHT
+
+=cut
+
+END_OF_DOCS
